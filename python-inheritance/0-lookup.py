8ea39c511699c7aa284c413b7657e0419f6d344f
Lenineng <l.ngenzi@alueducation.com>
2023-11-15 13:33:22 +0200

    more classes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""list of a vailable attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Return attributes
+    Args :
+        - obj: object to get attributes
+    """
+
+    return dir(obj)
